74322c5edd778fc65cc1d3ee6b376a8701be4aeb
luisEfe80 <luismendez80@hotmail.com>
2021-05-18 18:30:09 -0400

    content addition 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,6 +2,64 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        .h3
+          | 1
+      .h3 ¿Por qué enseñar educación STEM en el mundo?
+    p.mb-5 La educación STEM es relativamente nueva a nivel mundial. Al dar una mirada a los países con mejor desempeño en los resultados de las pruebas PISA: China, Taiwán, Japón, Singapur, Corea del Sur, Canadá, Alemania, Finlandia, Francia, Reino Unido, Brasil, Australia se puede ver cómo sus propuestas educativas nacionales tienen una relación directa entre la Ciencia, la Tecnología, la Ingeniería y las Matemáticas.
+    p.mb-5 En este sentido, la educación STEM tiene pertenencia en su implementación porque: 
+    .row.mb-5
+      .col-lg-5
+
+      .col-lg-7
+        ul.lista-ul
+          li 
+            i.fas.fa-check 
+            | A nivel mundial se considera a la Ciencia y la Tecnología como pilares de una economía sostenible y próspera.
+          li
+            li 
+              i.fas.fa-check 
+              | La educación STEM, con énfasis en la integración de las áreas, brinda una vía sólida para potencializar a los futuros ciudadanos hacia un desarrollo sostenible y próspero a todos los países.
+          li 
+            i.fas.fa-check 
+            | Se presenta una tendencia a la disminución del número de profesionales en las áreas de Ciencias, Tecnología e Ingeniería.
+          li 
+            i.fas.fa-check 
+            | Es fundamental aumentar la participación de la mujer en las áreas de desarrollo de los países, ya que su participación en los puestos de trabajo de liderazgo en Ciencia y Tecnología es muy baja.
+          li
+            i.fas.fa-check
+            | La educación STEM debe atraer mayor interés por parte de los estudiantes, pero en especial de las niñas a quienes se les debe impulsar su liderazgo en ciencia y tecnología.
+    
+    p.mb-5 Además, se puede tomar como referencia a estas justificaciones de por qué enseñar desde la educación STEM, el informe de ACOLA (Dobson, 2013), que es un foro anual que se realiza en Australia mediante el cual las academias y sus miembros asociados se unen para contribuir con el asesoramiento de expertos para informar la política nacional y desarrollar soluciones innovadoras a problemas globales complejos y necesidades nacionales emergentes. Este informe brinda un panorama actual de los países de Europa a partir de las siguientes afirmaciones: 
+    .row.mb-5
+      .col-lg-5
+
+      .col-lg-7
+        ul.lista-ul
+          li 
+            i.fas.fa-check 
+            | Puede decirse que la política y la práctica de la educación escolar en la ciencia viven en su propio mundo. A pesar de que existe la investigación europea sobre la enseñanza de la Ciencia, la educación en la Ciencia sigue teniendo una base empírica.
+          li
+            li 
+              i.fas.fa-check 
+              | Los estudiantes perciben la Ciencia como demasiado abstracta, porque se pretende enseñar ideas fundamentales sin el suficiente respaldo experimental, observacional e interpretativo y sin mostrar, adecuadamente, sus implicaciones y sin darles la oportunidad de un desarrollo acumulativo de su comprensión e importancia.
+          li 
+            i.fas.fa-check 
+            | Los programas de ciencias, son en su mayoría, excesivamente fácticos, en parte por el crecimiento vertiginoso del conocimiento científico y la continua adición de temas a una extensa gama de ellos.
+          li 
+            i.fas.fa-check 
+            | Es fundamental aumentar la participación de la mujer en las áreas de desarrollo de los países, ya que su participación en los puestos de trabajo de liderazgo en Ciencia y Tecnología es muy baja.
+          li
+            i.fas.fa-check
+            | Los contenidos y la forma de transmisión de enseñanza tradicionalmente establecida, tiende a distorsionar la comprensión del estudiante sobre la naturaleza de la Ciencia y del conocimiento, al ignorar los aspectos metodológicos de razonamiento y culturales de la Ciencia.
+    .row.mb-5
+      .col-lg-2.col-md-2
+      .col-lg-10.col-md-10
+        p.mb-4 En la actualidad, la implementación de la educación STEM tiene mayor interés por parte de algunos países, pero ¿cómo se viene desarrollando la educación STEM en el mundo? El desarrollo de planes en educación en cada país están sujetos a sus necesidades, su visión propia del futuro, su cultura y sus procesos sociopolíticos. 
+        p.mb-4 A continuación, se presenta el estudio realizado por el Gobierno de Australia denominado STEM, Technology, Engineering and Mathematics (STEM) Education (Marginson, 2013) de los países con mejores resultados en las pruebas PISA y los esfuerzos que vienen adelantando para la incorporación de la enseñanza STEM en su sistema educativo.
+    
+    p.mb-5 Se puede concluir, entonces, que los planes de educación en el mundo están sujetos a sus propias necesidades y visión del mundo, su cultura y los procesos sociopolíticos que desarrollan; se observa la necesidad de dar un cambio en la forma de enseñar en la educación que permita la mejora sustancial en cómo llevar el conocimiento a los estudiantes del siglo XXI, así, la educación STEM ofrece una solución importante al ámbito mundial.
 
     //- El componente "Muestras" tiene las aplicaciones de todos los componentes de la base
     //- se encuentra en src/components/Muestras.vue
---
+++ src/views/Tema4.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    .titulo-principal
+      .titulo-principal__numero
+        .h3
+          | 4
+      .h3 Planeación para la implementación de la educación STEM
+    .row.mb-5 
+      .col-lg-4
+
+      .col-lg-8
+        p.mb-4 Para desarrollar la planeación de la educación STEM en la institución educativa es importante considerar: ¿a dónde se quiere llegar con esta forma de enseñar y aprender? Para iniciar, se debe considerar la misión y la visión de la institución, así como los proyectos que en la actualidad viene desarrollando, los recursos humanos con los que actualmente cuenta y la disponibilidad de los equipos de docentes para acometer esta nueva propuesta educativa. Uno de los grandes fenómenos que se presentan en el campo de la educación es la dificultad por parte de docentes y estudiantes de trabajar en equipo y de manera colaborativa.
+        p.mb-4 El trabajo en equipo entre colegas es la clave para el éxito en docencia; además, la flexibilidad del docente a la hora de recibir u ofrecer apoyo de o hacia sus compañeros debe ser una constante para el desarrollo académico en las instituciones educativas. La educación STEM promueve que tanto docentes como estudiantes deben aprender a trabajar en equipo y saber explotar los beneficios de la colaboración.
+        p.mb-4 Finalmente, otro aspecto a considerar en sus actividades de aprendizaje es: ¿por qué enseñar con base en la resolución de preguntas o problemas? La educación STEM responde a cuestionamientos que se plantean a lo largo de la vida; es de recordar que en los días de colegio y universidad lo único que se pedía era la memorización de información, lo único que se evaluaba era una gran cantidad de datos retenidos al momento de la misma, obteniendo buenas y malas notas, pero, ¿hoy en día se recuerda esa información? Puede que algunos lo recuerden, pero la gran mayoría no. 
+    p.mb-5 En la actualidad la información se encuentra en internet y con solo un clic se puede tener la información de cualquier tema que se desee saber. Por eso es importante brindar a los nuevos ciudadanos del siglo XXI momentos educativos interdisciplinarios y para enseñar en forma innovadora se debe contar con herramientas adecuadas, pues el uso de herramientas tecnológicas hace más fácil la manera de enseñar de una forma significativa. Entonces, se puede concluir que las herramientas didácticas vistas, como fueron ABL y ABP, permiten iniciar un camino multidisciplinario o interdisciplinario con el fin de lograr el máximo de integración.
+    p.mb-5 Con todo lo anterior, ahora es importante entender el alcance de la implementación de la educación STEM según el proyecto filosófico y proyecto educativo de las instituciones de educación, por lo que en la siguiente sección se presentan algunas reflexiones. 
+    
+    #t_4_1.titulo-segundo
+      .h4 4.1 El alcance de la educación STEM y el PEI y el currículo
+    .row
+      .col-lg-4
+        
+      .col-lg-8
+        p.mb-4 En todo establecimiento educativo el Proyecto Educativo Institucional y su currículo tienen un periodo de tiempo donde se pretenden alcanzar unos objetivos para un tiempo específico y al llegar el momento de su evaluación para ser actualizado, es posible que la visión y la misión de la institución sufran cambios al igual que los proyectos institucionales para que no pierdan vigencia.
+        p.mb-4 Es en ese momento cuando el equipo encargado de esta actividad decide la intensidad horaria de cada una de las asignaturas y cómo transformar el Plan de Estudios para la vinculación de la educación STEM, garantizando que se cumplan los requerimientos mínimos exigidos por los entes nacionales, dado que los estudiantes deben recibir los temas exigidos y cumplir con las evaluaciones establecidas.
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema2',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped></style>
